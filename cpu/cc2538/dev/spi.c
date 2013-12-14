cfb7eaf0fcb0a98878ded17eed7edc9cb69ad95e
Samuel Jacob <samueldotj@gmail.com>
2013-12-13 22:34:23 -0800

    Fixed a build breakage caused by recent merge.

--- cpu/cc2538/dev/spi.c
+++ cpu/cc2538/dev/spi.c
@@ -42,6 +42,23 @@
 #include "dev/ssi.h"
 #include "dev/gpio.h"
 
+/*---------------------------------------------------------------------------*/
+/**
+ * \name SPI configuration
+ *
+ * These values configure which CC2538 pins to use for the SPI lines.
+ * @{
+ */
+#define SPI_CLK_PORT             GPIO_A_NUM
+#define SPI_CLK_PIN              2
+#define SPI_MOSI_PORT            GPIO_A_NUM
+#define SPI_MOSI_PIN             4
+#define SPI_MISO_PORT            GPIO_A_NUM
+#define SPI_MISO_PIN             5
+#define SPI_SEL_PORT             GPIO_B_NUM
+#define SPI_SEL_PIN              5
+/** @} */
+
 #define SPI_CLK_PORT_BASE        GPIO_PORT_TO_BASE(SPI_CLK_PORT)
 #define SPI_CLK_PIN_MASK         GPIO_PIN_MASK(SPI_CLK_PIN)
 #define SPI_MOSI_PORT_BASE       GPIO_PORT_TO_BASE(SPI_MOSI_PORT)
