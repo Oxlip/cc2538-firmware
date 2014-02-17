7bf5a48e46aa251da5b6560c5256ce2c1482f459
Samuel Jacob <samueldotj@gmail.com>
2014-02-16 19:20:01 -0800

    Removed trialing spaces.

--- platform/plugz-switch/dimmer.c
+++ platform/plugz-switch/dimmer.c
@@ -52,15 +52,15 @@ dimmer_init()
    /* TODO: Do we need to dynamically calculate the frequency? */
 
    /* The number of times Zero crossing interrupt will be called per second
-    *, for 50Hz, it's 100 times */ 
+    *, for 50Hz, it's 100 times */
    zero_crossing_frequency = 2 * REGIONAL_VOLTAGE_FREQUENCY;
-   
+
    /* Time in microseconds between Zero cross Interrupts, here it's 10 pow
     * 4 */
    zero_crossing_interval_micro_second = 1000000UL / zero_crossing_frequency;
 
    /* Time in microseconds when we divide the interval into 100 equal parts
-    * Here it would be 100 micro seconds */ 
+    * Here it would be 100 micro seconds */
    dimmer_callback_granularity_micro_second =
       zero_crossing_interval_micro_second/100;
 
@@ -79,7 +79,7 @@ void dimmer_callback( struct rtimer *rt, void* ptr)
 
 /*
  * When the Zero Cross circuit detects AC sine wave's zero cross it generates an
- * interrupt which is handled by an ISR which calls this function. 
+ * interrupt which is handled by an ISR which calls this function.
  * So this function will be called at 50Hz frequency(100 times per second).
  */
 void
@@ -91,15 +91,15 @@ zero_cross_handler(uint8_t port, uint8_t pin)
    static int intr_count = 0;
 
    intr_count++;
-   /* Print every 5 seconds */ 
+   /* Print every 5 seconds */
    if (intr_count % 500)
-      PRINTF("zero_cross_handler invoked %d times, port = %d, pin = %d\n", 
+      PRINTF("zero_cross_handler invoked %d times, port = %d, pin = %d\n",
             intr_count, port,pin);
-#endif /* DEBUG */   
+#endif /* DEBUG */
    for(i = 0; i< MAX_TRIACS; i++)
    {
       /* For Dim percentage of 100 we don't start the timer,
-       *  for the rest the timer fires at the closest approximation. 
+       *  for the rest the timer fires at the closest approximation.
        */
       if(dimmer_config[i].enabled == 1 && dimmer_config[i].percent != 100)
       {
@@ -107,7 +107,7 @@ zero_cross_handler(uint8_t port, uint8_t pin)
          rtimer_expire = (dimmer_callback_granularity_micro_second *
                dimmer_config[i].percent) / rt_clock_time_in_microseconds;
 
-         if( RTIMER_OK != rtimer_set(&dimmer_config[i].rt, 
+         if( RTIMER_OK != rtimer_set(&dimmer_config[i].rt,
                   RTIMER_NOW() + rtimer_expire + 2, 1,
                   (rtimer_callback_t)dimmer_callback, NULL))
          {
@@ -133,7 +133,7 @@ void dimmer_enable(int triac, int percent)
    }
 
    dimmer_config[triac].percent = percent;
-   
+
    if(dimmer_config[triac].percent == 100)
          plugz_triac_turn_off(triac);
 
