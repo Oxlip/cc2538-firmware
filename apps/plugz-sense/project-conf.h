0a618f782c992f71547784a9652b0fe27b86075f
Samuel Jacob <samueldotj@gmail.com>
2014-10-23 21:39:47 -0700

    Removed plugz-sense and plugz-switch directories.

--- apps/plugz-sense/project-conf.h
+++
@@ -1,8 +0,0 @@
-#ifndef __PROJECT_USENSE_CONF_H__
-#define __PROJECT_USESE_CONF_H__
-
-/* Multiplies with chunk size, be aware of memory constraints. */
-#undef COAP_MAX_OPEN_TRANSACTIONS
-#define COAP_MAX_OPEN_TRANSACTIONS   4
-
-#endif
